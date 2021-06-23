05954be77edd39c5af41096bd5cb6117de079bf4
Philip HYW <philiphyw@outlook.com>
2021-06-22 21:03:41 -0400

    updated 03review

--- p05porjectfromscratch/src/03review/App.js
+++ p05porjectfromscratch/src/03review/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Review from './Review';
 import {FaGithubSquare} from 'react-icons/fa';
+import './index.css';
 
 
 function App() {
   return <main>
     <section className="container">
       <div className="title">
-        <h2>our reviews</h2>
+        <h2>our reviews <FaGithubSquare /></h2>
         <div className="underline"></div>
       </div>
     </section>
--- p05porjectfromscratch/src/03review/Review.js
+++ p05porjectfromscratch/src/03review/Review.js
@@ -1,10 +1,63 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  
-  return <h2>{people[0].name}</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+  const maxIndex = people.length>0?people.length-1:0;
+  const decreIndex = () => {
+    if (index >= 1) {
+      setIndex(index - 1);
+    } else {
+      setIndex(0);
+    }
+  };
+
+  const increIndex = () => {
+    if (!people || maxIndex === 0 || index >= maxIndex) {
+      setIndex(index);
+    } else {
+      setIndex(index + 1);
+    }
+  };
+
+  const randomIndex = ()=>{
+    let rdIndex = Math.floor(Math.random() *  (maxIndex+1));
+    setIndex(rdIndex);
+  }
+
+
+  return (
+    <>
+      <div className="review">
+        <h2>{name}</h2>
+        <div className="img-container">
+          <img
+            src={image}
+            className="person-img"
+            alt={name}
+          ></img>
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="button-container">
+          <button onClick={() => decreIndex()} className="prev-btn">
+            <FaChevronLeft />
+          </button>
+          <button onClick={() => increIndex()} className="next-btn">
+            <FaChevronRight />
+          </button>
+        </div>
+        <button onClick={() => randomIndex()} className="random-btn">
+            Random
+          </button>
+      </div>
+    </>
+  );
 };
 
 export default Review;
